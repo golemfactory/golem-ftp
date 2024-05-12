c298ea1f25ef32438240bf9acd5113b92869601e
scx1332 <sieciech.czajka@golem.network>
2024-05-12 22:17:55 +0200

    chore: changed how output is parsed

--- integration/qos_plan.sh
+++ integration/qos_plan.sh
@@ -1,8 +1,17 @@
 # Script for planning QoS for the integration test
-docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 10.3% 25% delay 200ms 50ms distribution normal
-sleep 60
+sleep 30
 docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 1.3% 25% delay 200ms 50ms distribution normal
 docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 1.3% 25% delay 200ms 50ms distribution normal
-sleep 60
-docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 0.3% 25% delay 20ms
-docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 0.3% 25% delay 20ms
+sleep 30
+docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 10.3% 25% delay 200ms 50ms distribution normal
+docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 10.3% 25% delay 200ms 50ms distribution normal
+sleep 30
+docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 20.3% 25% delay 200ms 50ms distribution normal
+docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 20.3% 25% delay 200ms 50ms distribution normal
+sleep 30
+docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 30.3% 25% delay 200ms 50ms distribution normal
+docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 30.3% 25% delay 200ms 50ms distribution normal
+sleep 30
+docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 40.3% 25% delay 200ms 50ms distribution normal
+docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 40.3% 25% delay 200ms 50ms distribution normal
+sleep 30
