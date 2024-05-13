7b6689f14fd5b50966beab67193fe596152817ee
scx1332 <sieciech.czajka@golem.network>
2024-05-13 22:02:40 +0200

    chore: working on multiple GSB_URL separation

--- integration/qos_plan5.sh
+++ integration/qos_plan5.sh
@@ -1,7 +1,6 @@
 # Script for planning QoS for the integration test
 docker exec integration-yagna1-1 tc qdisc add dev eth0 root tbf rate 50.0mbit burst 15k latency 25ms
 docker exec integration-yagna2-1 tc qdisc add dev eth0 root tbf rate 50.0mbit burst 15k latency 25ms
-sleep 30
-docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 0% 25% delay 200ms 50ms distribution normal
-docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 0% 25% delay 200ms 50ms distribution normal
+docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 0% 25% delay 100ms 50ms distribution normal
+docker exec integration-yagna1-2 tc qdisc change dev eth0 root netem loss 0% 25% delay 100ms 50ms distribution normal
 sleep 30
