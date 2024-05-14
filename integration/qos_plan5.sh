f642a67f331d37026cf1b5230e8d2303facadca6
scx1332 <sieciech.czajka@golem.network>
2024-05-14 11:33:49 +0200

    chore: setting net type

--- integration/qos_plan5.sh
+++ integration/qos_plan5.sh
@@ -1,6 +1,7 @@
 # Script for planning QoS for the integration test
 #docker exec integration-yagna1-1 tc qdisc add dev eth0 root tbf rate 50.0mbit burst 15k latency 25ms
 #docker exec integration-yagna2-1 tc qdisc add dev eth0 root tbf rate 50.0mbit burst 15k latency 25ms
-docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 0% 25% delay 50ms 5ms distribution normal
-docker exec integration-yagna2-1 tc qdisc change dev eth0 root netem loss 0% 25% delay 50ms 5ms distribution normal
+docker exec integration-central-1 tc qdisc change dev eth0 root netem loss 0% 25% delay 50ms 1ms distribution normal
+#docker exec integration-yagna1-1 tc qdisc change dev eth0 root netem loss 0% 25% delay 50ms 5ms distribution normal
+#docker exec integration-yagna2-1 tc qdisc change dev eth0 root netem loss 0% 25% delay 50ms 5ms distribution normal
 sleep 30
